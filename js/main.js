81b8636ac7f0f14d43dd46422769811a4563980e
Tyler <44188028+twit96@users.noreply.github.com>
2021-08-08 19:41:53 -0500

    Swapped Intro Taglines Anim from JS to CSS

--- js/main.js
+++ js/main.js
@@ -49,22 +49,6 @@ logo_lg.onload = function() {
   main_logo.classList.remove("blurred");
 }
 
-// cycle through taglines
-var taglines = intro.getElementsByClassName("tagline");
-var active_idx = 0;
-var active_tag = taglines[0];
-setTimeout(updateTag, 3000)
-
-function updateTag() {
-  active_tag.classList.remove("active");
-  active_idx = (active_idx + 1) % taglines.length;
-  active_tag = taglines[active_idx];
-  active_tag.classList.add("active");
-  setTimeout(function() {
-    updateTag();
-  }, 3000);
-}
-
 // add randomly generated elements to background
 var rand_span;  // placed in #intro
 var clone;      // placed in #header-bg
