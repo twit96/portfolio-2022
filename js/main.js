f6c0a03ff540377b21f3ae42c7e75e8e2cefa0fe
Tyler <44188028+twit96@users.noreply.github.com>
2021-08-31 15:21:00 -0500

    test - remove randomly generated elements

--- js/main.js
+++ js/main.js
@@ -66,91 +66,91 @@ function updateTag() {
 }
 
 // add randomly generated elements to background
-var rand_span;  // placed in #intro
-var clone;      // placed in #header-bg
-
-// random properties
-var rand_x;
-var rand_y;
-var rand_size;
-var rand_rotation;
-var rand_opacity;
-
-// random leaves
-for (i=0; i<30; i++) {
-  rand_span = document.createElement("span");
-  rand_span.classList.add("leaf");
-  // generate random attributes
-  rand_x = Math.floor(Math.random() * 100);         // 0 to 100 vw
-  rand_y = Math.floor(Math.random() * 100);         // 0 to 100 vh
-  rand_size = Math.floor(Math.random() * 15) + 10;  // 10 to 25 vmin
-  rand_rotation = Math.floor(Math.random() * 360);  // 0 to 360 deg
-  // set CSS styles
-  rand_span.style.left = rand_x + "vw";
-  rand_span.style.top = rand_y + "vh";
-  rand_span.style.width = rand_size + "vmin";
-  rand_span.style.height = rand_size + "vmin";
-  rand_span.style.transform = "rotate(" + rand_rotation + "deg)";
-  // place on page
-  intro.insertAdjacentElement("beforeend", rand_span);
-  clone = rand_span.cloneNode(true);
-  loader.insertAdjacentElement("beforeend", clone);
-}
-
-// random orbs
-for (i=0; i<30; i++) {
-  rand_span = document.createElement("span");
-  rand_span.classList.add("orb");
-  // generate random attributes
-  rand_x = Math.floor(Math.random() * 100);         // 0 to 100 vw
-  rand_y = Math.floor(Math.random() * 100);         // 0 to 100 vh
-  rand_size = Math.floor(Math.random() * 2) + 1;    // 1 to 2 vmin
-  rand_opacity = (Math.random() * 0.75) + 0.25;	    // 0.15 to 1
-  // set CSS styles
-  rand_span.style.left = rand_x + "vw";
-  rand_span.style.top = rand_y + "vh";
-  rand_span.style.width = rand_size + "vmin";
-  rand_span.style.height = rand_size + "vmin";
-  rand_span.style.filter = "blur(" + (rand_size / 2) + "vmin)";
-  rand_span.style.opacity = rand_opacity;
-  // place on page
-  intro.insertAdjacentElement("beforeend", rand_span);
-  clone = rand_span.cloneNode(true);
-  header_bg.insertAdjacentElement("beforeend", clone);
-}
+// var rand_span;  // placed in #intro
+// var clone;      // placed in #header-bg
+//
+// // random properties
+// var rand_x;
+// var rand_y;
+// var rand_size;
+// var rand_rotation;
+// var rand_opacity;
+//
+// // random leaves
+// for (i=0; i<30; i++) {
+//   rand_span = document.createElement("span");
+//   rand_span.classList.add("leaf");
+//   // generate random attributes
+//   rand_x = Math.floor(Math.random() * 100);         // 0 to 100 vw
+//   rand_y = Math.floor(Math.random() * 100);         // 0 to 100 vh
+//   rand_size = Math.floor(Math.random() * 15) + 10;  // 10 to 25 vmin
+//   rand_rotation = Math.floor(Math.random() * 360);  // 0 to 360 deg
+//   // set CSS styles
+//   rand_span.style.left = rand_x + "vw";
+//   rand_span.style.top = rand_y + "vh";
+//   rand_span.style.width = rand_size + "vmin";
+//   rand_span.style.height = rand_size + "vmin";
+//   rand_span.style.transform = "rotate(" + rand_rotation + "deg)";
+//   // place on page
+//   intro.insertAdjacentElement("beforeend", rand_span);
+//   clone = rand_span.cloneNode(true);
+//   loader.insertAdjacentElement("beforeend", clone);
+// }
+//
+// // random orbs
+// for (i=0; i<30; i++) {
+//   rand_span = document.createElement("span");
+//   rand_span.classList.add("orb");
+//   // generate random attributes
+//   rand_x = Math.floor(Math.random() * 100);         // 0 to 100 vw
+//   rand_y = Math.floor(Math.random() * 100);         // 0 to 100 vh
+//   rand_size = Math.floor(Math.random() * 2) + 1;    // 1 to 2 vmin
+//   rand_opacity = (Math.random() * 0.75) + 0.25;	    // 0.15 to 1
+//   // set CSS styles
+//   rand_span.style.left = rand_x + "vw";
+//   rand_span.style.top = rand_y + "vh";
+//   rand_span.style.width = rand_size + "vmin";
+//   rand_span.style.height = rand_size + "vmin";
+//   rand_span.style.filter = "blur(" + (rand_size / 2) + "vmin)";
+//   rand_span.style.opacity = rand_opacity;
+//   // place on page
+//   intro.insertAdjacentElement("beforeend", rand_span);
+//   clone = rand_span.cloneNode(true);
+//   header_bg.insertAdjacentElement("beforeend", clone);
+// }
 
 
 // Demo Functionality ---------------------------------------------------------
 var demo = document.getElementById("demo");
 
 // add randomly generated elements to background
-var rand_span;  // placed in #intro
-
-// random properties
-var rand_x;
-var rand_y;
-var rand_size;
-var rand_rotation;
-var rand_opacity;
-
-// random squares
-for (i=0; i<15; i++) {
-  rand_span = document.createElement("span");
-  rand_span.classList.add("square");
-  // generate random attributes
-  rand_x = Math.floor(Math.random() * 100) - 7.5;     // -7.5 to 92.5 vw
-  rand_y = Math.floor(Math.random() * 85) + 7.5;      // 7.5 to 92.5 vh
-  rand_size = Math.floor(Math.random() * 15) + 15;  	// 15 to 30 vmin
-  rand_rotation = Math.floor(Math.random() * 360);  	// 0 to 360 deg
-  // set CSS styles
-  rand_span.style.left = rand_x + "%";
-  rand_span.style.top = rand_y + "%";
-  rand_span.style.width = rand_size + "vmin";
-  rand_span.style.height = rand_size + "vmin";
-  rand_span.style.transform = "rotate(" + rand_rotation + "deg)";
-  // place on page
-  demo.insertAdjacentElement("beforeend", rand_span);
-}
+// var rand_span;  // placed in #intro
+//
+// // random properties
+// var rand_x;
+// var rand_y;
+// var rand_size;
+// var rand_rotation;
+// var rand_opacity;
+//
+// // random squares
+// for (i=0; i<15; i++) {
+//   rand_span = document.createElement("span");
+//   rand_span.classList.add("square");
+//   // generate random attributes
+//   rand_x = Math.floor(Math.random() * 100) - 7.5;     // -7.5 to 92.5 vw
+//   rand_y = Math.floor(Math.random() * 85) + 7.5;      // 7.5 to 92.5 vh
+//   rand_size = Math.floor(Math.random() * 15) + 15;  	// 15 to 30 vmin
+//   rand_rotation = Math.floor(Math.random() * 360);  	// 0 to 360 deg
+//   // set CSS styles
+//   rand_span.style.left = rand_x + "%";
+//   rand_span.style.top = rand_y + "%";
+//   rand_span.style.width = rand_size + "vmin";
+//   rand_span.style.height = rand_size + "vmin";
+//   rand_span.style.transform = "rotate(" + rand_rotation + "deg)";
+//   // place on page
+//   demo.insertAdjacentElement("beforeend", rand_span);
+// }
 
 var figure = demo.querySelector("figure");
 
