f70682f1c3d0ff95ffab6def47f0dd5b4b2554ea
Tyler <44188028+twit96@users.noreply.github.com>
2021-09-19 23:57:45 -0500

    Split main.css into home.css
    moved styles specific to the homepage into their own file to reduce bloat on other pages

--- projects/index.php
+++ projects/index.php
@@ -6,7 +6,7 @@
 		<meta name="description" content="Tyler Wittig's Projects" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="icon" href="/img/icon.png" />
-		<link rel="stylesheet" type="text/css" href="/css/main.css?v=20210915" />
+		<link rel="stylesheet" type="text/css" href="/css/main.css?v=20210919.01" />
     <link rel="stylesheet" type="text/css" href="/css/projects.css" />
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
