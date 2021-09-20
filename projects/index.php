341c3f261a5a7ddcdd783c2bcec2022136cd80d3
Tyler <44188028+twit96@users.noreply.github.com>
2021-09-20 17:01:23 -0500

    corrected link to main.js in projects page

--- projects/index.php
+++ projects/index.php
@@ -11,7 +11,7 @@
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
-    <script src="/js/header.js" defer></script>
+    <script src="/js/main.js" defer></script>
     <script src="/js/projects.js" defer></script>
   </head>
   <body>
