dac0780cee043a22c8962dbf9d3f887d8e4dadcf
Tyler <44188028+twit96@users.noreply.github.com>
2021-09-26 13:52:20 -0500

    Update admin_dashboard.php

--- php/admin_dashboard.php
+++ php/admin_dashboard.php
@@ -227,6 +227,20 @@ function doEngine() {
 
   // if user updated table
   } else if (isset($_POST["update"])) {
+    $server = "localhost";
+    $user   = "portfolio_user";
+    $pwd    = "portfolio_user_pass";
+    $dbName = "Portfolio";
+
+    // Connect to MySQL Server
+    $mysqli = new mysqli ($server, $user, $pwd, $dbName);
+    if ($mysqli->connect_errno) {
+      die('Connect Error: ' . $mysqli->connect_errno . ": " . $mysqli->connect_error);
+    }
+    // Select Database
+    $mysqli->select_db($dbName) or die($mysqli->error);
+
+    // update db
     echo '<script>alert("Update.");</script>';
     unset($_POST["update"]);
     buildDashboard($mysqli);
