3d085b1d0254048eb75997baeb6fb8170a2bab5b
Tyler <44188028+twit96@users.noreply.github.com>
2021-09-27 14:31:59 -0500

    Update admin_dashboard.php

--- php/admin_dashboard.php
+++ php/admin_dashboard.php
@@ -273,38 +273,38 @@ function updateImage($directory) {
     echo ''.$target_dir.'<br />'.$target_file.'<br />'.$imageFileType.'<br />';
     // Check if image file is a actual image or fake image
     $check = getimagesize($_FILES["image"]["tmp_name"]);
-    if($check !== false) {
-      echo '<script>console.log("File is an image - '.$check["mime"].'.");</script>';
+    if ($check !== false) {
+      echo 'File is an image - '.$check["mime"];
       $uploadOk = 1;
     } else {
-      echo '<script>console.log("File is not an image.");</script>';
+      echo 'File is not an image.';
       $uploadOk = 0;
     }
     // Check if file already exists
     if (file_exists($target_file)) {
-      echo '<script>console.log("Sorry, file already exists.");</script>';
+      echo 'Sorry, file already exists.';
       $uploadOk = 0;
     }
     // Check file size
     if ($_FILES["image"]["size"] > 500000) {
-      echo '<script>console.log("Sorry, your file is too large.");</script>';
+      echo 'Sorry, your file is too large.';
       $uploadOk = 0;
     }
     // Allow certain file formats
     if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+      echo 'Sorry, only JPG, JPEG, PNG & GIF files are allowed.';
       $uploadOk = 0;
     }
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-      echo '<script>console.log("Sorry, your file was not uploaded.");</script>';
+      echo 'Sorry, your file was not uploaded.';
     // if everything is ok, try to upload file
     } else {
       if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-        echo "<script>console.log(The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.);</script>";
-        return basename($_FILES["image"]["name"]);
+        echo 'The file '.htmlspecialchars( basename( $_FILES["image"]["name"])).' has been uploaded.';
+        return (basename($_FILES["image"]["name"]));
       } else {
-        echo "<script>console.log(Sorry, there was an error uploading your file.);</script>";
+        echo 'Sorry, there was an error uploading your file.';
         return 0;
       }
     }
