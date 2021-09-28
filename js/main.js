e3e392cff3b8a215fc904a1f5df3d780c6fa8c7c
Tyler <44188028+twit96@users.noreply.github.com>
2021-09-28 18:18:00 -0500

    Tweaked Orb Size and Opacity

--- js/main.js
+++ js/main.js
@@ -43,14 +43,13 @@ for (i=0; i<30; i++) {
   // generate random attributes
   rand_x = Math.floor(Math.random() * 100);         // 0 to 100 vw
   rand_y = Math.floor(Math.random() * 100);         // 0 to 100 vh
-  rand_size = Math.floor(Math.random() * 2) + 1;    // 1 to 2 vmin
-  rand_opacity = (Math.random() * 0.75) + 0.25;	    // 0.25 to 1
+  rand_size = (Math.random() * 1.5) + 0.5;          // 0.5 to 2 vmin
+  rand_opacity = (Math.random() * 0.5);	            // 0 to 0.5
   // set CSS styles
   rand_span.style.left = rand_x + "vw";
   rand_span.style.top = rand_y + "vh";
   rand_span.style.width = rand_size + "vmin";
   rand_span.style.height = rand_size + "vmin";
-  // rand_span.style.filter = "blur(" + (rand_size / 2) + "vmin)";
   rand_span.style.opacity = rand_opacity;
   // place on page
   intro.insertAdjacentElement("beforeend", rand_span);
