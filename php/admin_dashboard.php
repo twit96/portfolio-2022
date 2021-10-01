a027866b4a62c52bdddf36303dff9a9b8a6b5c76
Tyler <44188028+twit96@users.noreply.github.com>
2021-09-30 20:14:44 -0500

    tweaked command
    removed quotes where they shouldn't be

--- php/admin_dashboard.php
+++ php/admin_dashboard.php
@@ -414,12 +414,12 @@ function insertDB($mysqli, $row, $new_img_name) {
   $col_vals_length = count($col_vals);
 
   // build sql command
-  $command = 'INSERT INTO projects VALUES ';
-  $command .= '("'.$col_vals[0].'", ';
+  $command = "INSERT INTO projects VALUES ";
+  $command .= "(".$col_vals[0].", ";
   for ($i=1; $i<$col_vals_length-2; $i++) {
-    $command .= '"'.$col_vals[$i].'",';
+    $command .= "'".$col_vals[$i]."', ";
   }
-  $command .= '"'.$col_vals[$col_vals_length-1].'");';
+  $command .= $col_vals[$col_vals_length-1].");";
 
   echo $command;
   // $result = $mysqli->query($command);
