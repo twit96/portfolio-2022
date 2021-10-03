3278406fe2cdfa5ccabf9b10c5104483172be9cb
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-03 16:43:13 -0500

    remove meta theme-color
    deciding not to use

--- admin-dashboard/index.php
+++ admin-dashboard/index.php
@@ -6,7 +6,6 @@
 		<meta charset="utf-8" />
 		<meta name="description" content="Tyler Wittig's Portfolio Admin Dashboard" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="theme-color" content="#000000" />
 		<!-- Social Media Card -->
 		<meta name="author" content="Tyler Wittig" />
 		<meta property="og:title" content="Tyler Wittig | Admin Dashboard" />
