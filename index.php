3e3440102b92b768e68e076a76f0fb6c23386a06
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-10 14:11:45 -0500

    Updated Homepage with Email Link

--- index.php
+++ index.php
@@ -52,16 +52,18 @@
       <div class="details">
         <h1>Tyler Wittig</h1>
         <h2>Web Developer</h2>
-				<a href="https://github.com/twit96" target="_blank" rel="noreferrer">
-					<img src="/img/logo_github.png" alt="github logo" />
-				</a>
-				<a href="https://www.linkedin.com/in/tylerwittig/" target="_blank" rel="noreferrer">
-					<img src="/img/logo_linkedin.png" alt="linkedin logo" />
-				</a>
-				<a href="https://twitter.com/tyler_wittig" target="_blank" rel="noreferrer">
-					<img src="/img/logo_twitter.png" alt="twitter logo" />
-				</a>
-      </div>
+				<div class="links">
+					<a href="mailto:tylerwittig@utexas.edu?subject=Web%20Developer%20Information" target="_blank" rel="noreferrer">
+						<img src="/img/logo_email.png" alt="twitter logo" />
+					</a>
+					<a href="https://github.com/twit96" target="_blank" rel="noreferrer">
+						<img src="/img/logo_github.png" alt="github logo" />
+					</a>
+					<a href="https://www.linkedin.com/in/tylerwittig/" target="_blank" rel="noreferrer">
+						<img src="/img/logo_linkedin.png" alt="linkedin logo" />
+					</a>
+				</div>  <!-- ./links -->
+      </div>  <!-- ./details -->
 		</section>
 
 
