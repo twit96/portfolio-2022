8fc99d6173a9104eb0d9541caae4cefcd0e18322
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-15 11:40:16 -0500

    Revert "TEST - tagline animation moved from JS to CSS"
    This reverts commit a3e1ae78c2c76838c407752bfe6e371c83323755.

--- js/home.js
+++ js/home.js
@@ -12,6 +12,22 @@ logo_lg.onload = function() {
   main_logo.classList.remove("blurred");
 }
 
+// cycle through taglines
+var taglines = intro.getElementsByClassName("tagline");
+var active_idx = 0;
+var active_tag = taglines[0];
+setTimeout(updateTag, 3000)
+
+function updateTag() {
+  active_tag.classList.remove("active");
+  active_idx = (active_idx + 1) % taglines.length;
+  active_tag = taglines[active_idx];
+  active_tag.classList.add("active");
+  setTimeout(function() {
+    updateTag();
+  }, 3000);
+}
+
 
 // Featured Projects Section --------------------------------------------------
 var featured = document.getElementById("featured");
