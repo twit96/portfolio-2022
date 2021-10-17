ea8f7c28911fa92f70dbd28201f4c824afa3c879
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-16 22:26:42 -0500

    removed map tags from projects head

--- projects/index.php
+++ projects/index.php
@@ -3,10 +3,6 @@
 include('../templates/head.php');
 echo <<<HEAD_END
 \n
-    <!-- map -->
-    <script src='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js'></script>
-    <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />
-    <!--  -->
   </head>
 	<body>
 HEAD_END;
