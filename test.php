dee92304d5b04d0cab7ac58493a56939cad2c677
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-16 20:15:29 -0500

    Update test.php

--- test.php
+++ test.php
@@ -2,12 +2,18 @@
   $curr_dir = basename(getcwd());
   if ($curr_dir == 'html') {
     $page_name = 'Web Developer';
+    $og_url_extension = '';
     $og_description_extension = '';
   }
   else {
     $page_name = ucfirst($curr_dir);
-    $og_description_extension = '- '.$page_name;
+    $og_description_extension = ' - '.$page_name;
+    $og_description_extension = $page_name;
   }
+
+  $timestamp = function($file_path) {
+    return date('Ymd-His',filectime($file_path);
+  };
 
   echo <<<TOP
   <!DOCTYPE html>
@@ -18,16 +24,16 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <!-- Social Media Card -->
   		<meta name="author" content="Tyler Wittig" />
-      <meta property="og:title" content="Tyler Wittig | {$page_name} />;
+      <meta property="og:title" content="Tyler Wittig | {$page_name} />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://tylerwittig.com/{$page_name}/ />'
+      <meta property="og:url" content="https://tylerwittig.com/{$og_url_extension}/ />
   		<meta property="og:image" content="/img/site-card.png" />
-  		<meta property="og:description" content="Tyler Wittig's Portfolio Website {$og_description_extension}" />
+  		<meta property="og:description" content="Tyler Wittig's Portfolio Website{$og_description_extension}" />
       <!-- make the og:image larger -->
   		<meta name="twitter:card" content="summary_large_image">
   		<!-- end of social media card -->
       <link rel="icon" href="/img/icon.png" />
-  		<link rel="stylesheet" type="text/css" href="/css/main.css?v={date('Ymd-His',filectime('/css/main.css'))}" />
+  		<link rel="stylesheet" type="text/css" href="/css/main.css?v={$timestamp('/css/main.css')}" />
   TOP;
 
   echo '</head>';
