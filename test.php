351bbf93a52fd8d34d535903e280a6fcd381eb21
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-16 19:35:29 -0500

    Create test.php

---
+++ test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Test</title>
+  </head>
+  <body>
+    <?php
+
+    echo 'dirname(__FILE__)' . dirname(__FILE__) . '\n';
+    echo 'getcwd();' . getcwd(); . '\n';
+    echo 'basename(getcwd())' . basename(getcwd()) . '\n';
+    echo 'basename(__DIR__)' . basename(__DIR__) . '\n';
+
+     ?>
+  </body>
+</html>
