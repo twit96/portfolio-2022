3963209edf3b63370d9e49dada742ee3163488c9
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-16 21:31:16 -0500

    Properly formatted admin php file

--- admin/index.php
+++ admin/index.php
@@ -1,18 +1,24 @@
 <?php
-include('../templates/head.php');
-?>
-		<!-- Additional Head Tags -->
-		<meta name="description" content="Tyler Wittig's Portfolio Admin Dashboard" />
-		<link rel="stylesheet" type="text/css" href="/css/admin.css" />
-	</head>
 
-  <body>
-		<?php
-			include('../templates/header.html');
-			echo '<section id="entries">';
-			include('../php/admin_dashboard.php');
-			echo '</section>';
-			include('../templates/footer.html');
-		?>
+include('../templates/head.php');
+
+echo <<<HEAD_END
+    <!-- Additional Head Tags -->
+    <meta name="description" content="Tyler Wittig's Portfolio Admin Dashboard" />
+    <link rel="stylesheet" type="text/css" href="/css/admin.css" />
+  </head>
+	<body>
+HEAD_END;
+
+include('../templates/header.html');
+echo '<section id="entries">';
+include('../php/admin_dashboard.php');
+echo '</section>';
+include('../templates/footer.html');
+
+echo <<<PAGE_END
   </body>
 </html>
+PAGE_END;
+
+?>
