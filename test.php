bf7316999839f5cfd6bd35726d01749b8dc634c8
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-16 20:30:49 -0500

    Update test.php

--- test.php
+++ test.php
@@ -15,7 +15,7 @@
   //   return date('Ymd-His',filectime(basename($file_path)));
   // }
   $timestamp = function() {
-    return date('Ymd-His',filectime(basename('/css/main.css')));
+    return date('Ymd',filectime(basename('/css/main.css')));
   }
 
   echo <<<TOP
