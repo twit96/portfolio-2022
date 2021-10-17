04b5d0c6582b71a8b4134275dc6b27cc294d8b3f
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-16 20:08:38 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,17 +1,39 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Test</title>
-  </head>
+<?php
+  $curr_dir = basename(getcwd());
+  if ($curr_dir == 'html') {
+    $page_name = 'Web Developer';
+    $og_description_extension = '';
+  }
+  else {
+    $page_name = ucfirst($curr_dir);
+    $og_description_extension = '- '.$page_name;
+  }
+
+  echo <<<TOP
+  <!DOCTYPE html>
+  <html lang="en" dir="ltr">
+    <head>
+      <title>Tyler Wittig | {$page_name}</title>;
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- Social Media Card -->
+  		<meta name="author" content="Tyler Wittig" />
+      <meta property="og:title" content="Tyler Wittig | {$page_name} />;
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://tylerwittig.com/{$page_name}/ />';
+  		<meta property="og:image" content="/img/site-card.png" />
+  		<meta property="og:description" content="Tyler Wittig's Portfolio Website {$og_description_extension}" />
+      <!-- make the og:image larger -->
+  		<meta name="twitter:card" content="summary_large_image">
+  		<!-- end of social media card -->
+      <link rel="icon" href="/img/icon.png" />
+  		<link rel="stylesheet" type="text/css" href="/css/main.css?v={date('Ymd-His',filectime('/css/main.css'))}" />
+  TOP;
+
+  echo '</head>';
+?>
+
   <body>
-    <?php
-
-    echo 'dirname(__FILE__)' . dirname(__FILE__) . '<br />';
-    echo 'getcwd();' . getcwd() . '<br />';
-    echo 'basename(getcwd())' . basename(getcwd()) . '<br />';
-    echo 'basename(__DIR__)' . basename(__DIR__) . '<br />';
-
-     ?>
+    <h1>Test</h1>
   </body>
 </html>
