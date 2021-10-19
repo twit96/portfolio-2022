caa28a1e6bc6be08310d9400727bc7f6b7d8b875
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-18 21:12:08 -0500

    Header Tabbing

--- templates/header.php
+++ templates/header.php
@@ -12,32 +12,32 @@ if (strlen(strstr($agent, 'Firefox')) > 0) { $browser = 'firefox'; }
 else { $browser = ''; }
 
 echo <<<HEADER
-<div id="loader"></div>
+\n    <div id="loader"></div>
 
-<label id="dark-toggle-wrap" for="dark-toggle"{$if($browser=='firefox',' style="display:none;"','')}>
-  <span class="hidden-text">Dark Mode Toggle</span>
-  <input id="dark-toggle" type="checkbox">
-  <span class="slider"></span>
-</label>
+    <label id="dark-toggle-wrap" for="dark-toggle"{$if($browser=='firefox',' style="display:none;"','')}>
+      <span class="hidden-text">Dark Mode Toggle</span>
+      <input id="dark-toggle" type="checkbox">
+      <span class="slider"></span>
+    </label>
 
-<header>
-  <span>
-    <a href="/">TW</a>
-  </span>
-  <nav>
-    <a href="/"{$if($curr_dir=='html',' class="active"','')}>Home</a>
-    <a href="/projects/"{$if($curr_dir=='projects',' class="active"','')}>Projects</a>
-    <a class="resume" href="/20211003-résumé-tylerwittig.pdf">
-      Résumé
-    </a>
-  </nav>
-  <div class="nav-toggle">
-    <span></span>
-    <span></span>
-    <span></span>
-  </div>
-  <div id="header-bg"></div>
-</header>
+    <header>
+      <span>
+        <a href="/">TW</a>
+      </span>
+      <nav>
+        <a href="/"{$if($curr_dir=='html',' class="active"','')}>Home</a>
+        <a href="/projects/"{$if($curr_dir=='projects',' class="active"','')}>Projects</a>
+        <a class="resume" href="/20211003-résumé-tylerwittig.pdf">
+          Résumé
+        </a>
+      </nav>
+      <div class="nav-toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div id="header-bg"></div>
+    </header>
 HEADER;
 
 
