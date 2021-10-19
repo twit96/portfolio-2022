b9a2c0ee9fb6628ddb100c4add86fd20cfd6afe0
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-18 22:00:29 -0500

    All Projects Tabbing and Spacing

--- php/all_projects.php
+++ php/all_projects.php
@@ -9,7 +9,7 @@ ini_set("display_errors", "on");
 function displayFeatured($mysqli) {
   // Opening HTML
   echo <<<TOP
-  \n    <section id="projects">
+  \n\n    <section id="projects">
         <h2>Projects</h2>\n
   TOP;
 
@@ -20,46 +20,46 @@ function displayFeatured($mysqli) {
 
   while ($row = $result->fetch_assoc()) {
 
-    echo "\n".'     <article>';
-    echo "\n".'       <figure>';
+    echo '      <article>';
+    echo "\n".'        <figure>';
     if ($row['featured'] > 0) {
-      echo "\n".'         <div class="featured-badge">';
-      echo "\n".'           <span>#'.$row['featured'].'</span>';
-      echo "\n".'         </div>';
+      echo "\n".'          <div class="featured-badge">';
+      echo "\n".'            <span>#'.$row['featured'].'</span>';
+      echo "\n".'          </div>';
     }
-    echo "\n".'         <img src="/projects/'.$row['directory'].'/'.$row['image'].'" loading="lazy" alt="'.$row['title'].' Title Card" />';
-    echo "\n".'         <figcaption>'.$row['blurb'].'</figcaption>';
-    echo "\n".'       </figure>';
-    echo "\n".'       <div>';
-    echo "\n".'         <h3>'.$row['title'].'</h3>';
-    echo "\n".'         <em>'.$row['date'].'</em>';
-    echo "\n".'         <p>'.$row['description'].'</p>';
+    echo "\n".'          <img src="/projects/'.$row['directory'].'/'.$row['image'].'" loading="lazy" alt="'.$row['title'].' Title Card" />';
+    echo "\n".'          <figcaption>'.$row['blurb'].'</figcaption>';
+    echo "\n".'        </figure>';
+    echo "\n".'        <div>';
+    echo "\n".'          <h3>'.$row['title'].'</h3>';
+    echo "\n".'          <em>'.$row['date'].'</em>';
+    echo "\n".'          <p>'.$row['description'].'</p>';
 
     if (!is_null($row['primary_link']) && ($row['primary_link'] != "")) {
-      echo "\n".'         <a class="btn-text link" href="'.$row['primary_link'].'">';
-      echo "\n".'           <span class="icon"></span>';
-      echo "\n".'           '.$row['primary_link_text'];
-      echo "\n".'         </a>';
+      echo "\n".'          <a class="btn-text link" href="'.$row['primary_link'].'">';
+      echo "\n".'            <span class="icon"></span>';
+      echo "\n".'            '.$row['primary_link_text'];
+      echo "\n".'          </a>';
     }
     if (!is_null($row['secondary_link']) && ($row['secondary_link'] != "")) {
-      echo "\n".'         <a class="btn-text link" href="'.$row['secondary_link'].'">';
-      echo "\n".'           <span class="icon"></span>';
-      echo "\n".'           '.$row['secondary_link_text'];
-      echo "\n".'         </a>';
+      echo "\n".'          <a class="btn-text link" href="'.$row['secondary_link'].'">';
+      echo "\n".'            <span class="icon"></span>';
+      echo "\n".'            '.$row['secondary_link_text'];
+      echo "\n".'          </a>';
     }
     if (!is_null($row['tertiary_link']) && ($row['tertiary_link'] != "")) {
-      echo "\n".'         <a class="btn-text link" href="'.$row['tertiary_link'].'">';
-      echo "\n".'           <span class="icon"></span>';
-      echo "\n".'           '.$row['tertiary_link_text'];
-      echo "\n".'         </a>';
+      echo "\n".'          <a class="btn-text link" href="'.$row['tertiary_link'].'">';
+      echo "\n".'            <span class="icon"></span>';
+      echo "\n".'            '.$row['tertiary_link_text'];
+      echo "\n".'          </a>';
     }
 
-    echo "\n".'        </div>';
-    echo "\n".'      </article>';
+    echo "\n".'         </div>';
+    echo "\n".'       </article>';
   }
 
   // Closing HTML
-  echo "\n".'     </section>';
+  echo "\n".'     </section>'."\n\n";
 }
 
 
