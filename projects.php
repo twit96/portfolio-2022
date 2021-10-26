5bc4ae0e59d52971c89c6ca4cbf8921af4de6961
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-26 16:46:28 -0500

    Remove test.php - Add projects/index.php Copy (projects.php) to root

---
+++ projects.php
@@ -0,0 +1,18 @@
+<?php
+
+include('./templates/head.php');
+echo <<<HEAD_END
+\n  </head>
+  <body>
+HEAD_END;
+
+include('./templates/header.php');
+include('./php/all_projects.php');
+include('./templates/footer.php');
+
+echo <<<PAGE_END
+  </body>
+</html>
+PAGE_END;
+
+?>
