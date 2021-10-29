35f2605ab010e47a488c500845a974c5b9b2bd4c
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-29 14:23:03 -0500

    Finalized Articles Page Layout

--- misc/test-layouts/articles.php
+++ misc/test-layouts/articles.php
@@ -14,153 +14,393 @@ include('../../includes/header.php');
 
     <section id="articles">
       <h2>Articles</h2>
-      <article>
-        <figure>
-          <a href="#">
-            <img src="/img/projects/first-portfolio/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
-          </a>
-          <figcaption>Static Portfolio Website</figcaption>
-        </figure>
-        <div>
-          <h3>
-            <a href="#">
-              First Portfolio Website is First that is on the Web Never the
-              Last because it is First
-            </a>
-          </h3>
+      <div class="grid">
 
-          <p>
-            Serving as a playground for HTML, CSS, and JavaScript as I learned
-            more about web development, this static portfolio website underwent
-            numerous design changes and refactors in its time active.
-          </p>
-          <a class="btn-text link" href="#">
-            <span class="icon"></span>
-            Read Article
+        <article>
+          <a class="img-link" href="#">
+            <img src="/img/projects/connect-four/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
           </a>
+          <div class="details">
+            <ul class="tags">
+              <li><a href="#">Web Game</a></li>
+              <li><a href="#">JavaScript</a></li>
+              <li><a href="#">Grid Traversal</a></li>
+              <li><a href="#">Responsive</a></li>
+            </ul>
+            <h3>
+              <a href="#">
+                First Portfolio Website is First that is on the Web Never the
+                Last because it is First
+              </a>
+            </h3>
+            <p>
+              Serving as a playground for HTML, CSS, and JavaScript as I learned
+              more about web development, this static portfolio website underwent
+              numerous design changes and refactors in its time active...
+            </p>
+          </div>
           <div class="author">
             <img src="/img/profile.jpg"/>
             <p>
-              <b>Posted by Tyler Wittig</b>
-              <span class="btn-text date">
-                <span class="icon"></span>
-                2020-09-01
-              </span>
+              Posted by <b>Tyler Wittig</b> on 2020-09-01
             </p>
           </div>
-        </div>
-      </article>
+        </article>
 
-      <article>
-        <figure>
-          <a href="#">
-            <img src="/img/projects/first-portfolio/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
+        <article>
+          <a class="img-link" href="#">
+            <img src="/img/projects/connect-four/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
           </a>
-          <figcaption>Static Portfolio Website</figcaption>
-        </figure>
-        <div>
-          <h3>
-            <a href="#">
-              First Portfolio Website is First that is on the Web Never the
-              Last because it is First
-            </a>
-          </h3>
-
-          <p>
-            Serving as a playground for HTML, CSS, and JavaScript as I learned
-            more about web development, this static portfolio website underwent
-            numerous design changes and refactors in its time active.
-          </p>
-          <a class="btn-text link" href="#">
-            <span class="icon"></span>
-            Read Article
-          </a>
+          <div class="details">
+            <ul class="tags">
+              <li><a href="#">Web Game</a></li>
+              <li><a href="#">JavaScript</a></li>
+              <li><a href="#">Grid Traversal</a></li>
+              <li><a href="#">Responsive</a></li>
+            </ul>
+            <h3>
+              <a href="#">
+                First Portfolio Website is First that is on the Web Never the
+                Last because it is First
+              </a>
+            </h3>
+            <p>
+              Serving as a playground for HTML, CSS, and JavaScript as I learned
+              more about web development, this static portfolio website underwent
+              numerous design changes and refactors in its time active...
+            </p>
+          </div>
           <div class="author">
             <img src="/img/profile.jpg"/>
             <p>
-              <b>Posted by Tyler Wittig</b>
-              <span class="btn-text date">
-                <span class="icon"></span>
-                2020-09-01
-              </span>
+              Posted by <b>Tyler Wittig</b> on 2020-09-01
             </p>
           </div>
-        </div>
-      </article>
+        </article>
 
-      <article>
-        <figure>
-          <a href="#">
-            <img src="/img/projects/first-portfolio/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
+        <article>
+          <a class="img-link" href="#">
+            <img src="/img/projects/connect-four/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
           </a>
-          <figcaption>Static Portfolio Website</figcaption>
-        </figure>
-        <div>
-          <h3>
-            <a href="#">
-              First Portfolio Website is First that is on the Web Never the
-              Last because it is First
-            </a>
-          </h3>
-
-          <p>
-            Serving as a playground for HTML, CSS, and JavaScript as I learned
-            more about web development, this static portfolio website underwent
-            numerous design changes and refactors in its time active.
-          </p>
-          <a class="btn-text link" href="#">
-            <span class="icon"></span>
-            Read Article
-          </a>
+          <div class="details">
+            <ul class="tags">
+              <li><a href="#">Web Game</a></li>
+              <li><a href="#">JavaScript</a></li>
+              <li><a href="#">Grid Traversal</a></li>
+              <li><a href="#">Responsive</a></li>
+            </ul>
+            <h3>
+              <a href="#">
+                First Portfolio Website is First that is on the Web Never the
+                Last because it is First
+              </a>
+            </h3>
+            <p>
+              Serving as a playground for HTML, CSS, and JavaScript as I learned
+              more about web development, this static portfolio website underwent
+              numerous design changes and refactors in its time active...
+            </p>
+          </div>
           <div class="author">
             <img src="/img/profile.jpg"/>
             <p>
-              <b>Posted by Tyler Wittig</b>
-              <span class="btn-text date">
-                <span class="icon"></span>
-                2020-09-01
-              </span>
+              Posted by <b>Tyler Wittig</b> on 2020-09-01
             </p>
           </div>
-        </div>
-      </article>
+        </article>
 
-      <article>
-        <figure>
-          <a href="#">
-            <img src="/img/projects/first-portfolio/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
+        <article>
+          <a class="img-link" href="#">
+            <img src="/img/projects/connect-four/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
           </a>
-          <figcaption>Static Portfolio Website</figcaption>
-        </figure>
-        <div>
-          <h3>
-            <a href="#">
-              First Portfolio Website is First that is on the Web Never the
-              Last because it is First
-            </a>
-          </h3>
-
-          <p>
-            Serving as a playground for HTML, CSS, and JavaScript as I learned
-            more about web development, this static portfolio website underwent
-            numerous design changes and refactors in its time active.
-          </p>
-          <a class="btn-text link" href="#">
-            <span class="icon"></span>
-            Read Article
-          </a>
+          <div class="details">
+            <ul class="tags">
+              <li><a href="#">Web Game</a></li>
+              <li><a href="#">JavaScript</a></li>
+              <li><a href="#">Grid Traversal</a></li>
+              <li><a href="#">Responsive</a></li>
+            </ul>
+            <h3>
+              <a href="#">
+                First Portfolio Website is First that is on the Web Never the
+                Last because it is First
+              </a>
+            </h3>
+            <p>
+              Serving as a playground for HTML, CSS, and JavaScript as I learned
+              more about web development, this static portfolio website underwent
+              numerous design changes and refactors in its time active...
+            </p>
+          </div>
           <div class="author">
             <img src="/img/profile.jpg"/>
             <p>
-              <b>Posted by Tyler Wittig</b>
-              <span class="btn-text date">
-                <span class="icon"></span>
-                2020-09-01
-              </span>
+              Posted by <b>Tyler Wittig</b> on 2020-09-01
             </p>
           </div>
-        </div>
-      </article>
+        </article>
+
+        <article>
+          <a class="img-link" href="#">
+            <img src="/img/projects/connect-four/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
+          </a>
+          <div class="details">
+            <ul class="tags">
+              <li><a href="#">Web Game</a></li>
+              <li><a href="#">JavaScript</a></li>
+              <li><a href="#">Grid Traversal</a></li>
+              <li><a href="#">Responsive</a></li>
+            </ul>
+            <h3>
+              <a href="#">
+                First Portfolio Website is First that is on the Web Never the
+                Last because it is First
+              </a>
+            </h3>
+            <p>
+              Serving as a playground for HTML, CSS, and JavaScript as I learned
+              more about web development, this static portfolio website underwent
+              numerous design changes and refactors in its time active...
+            </p>
+          </div>
+          <div class="author">
+            <img src="/img/profile.jpg"/>
+            <p>
+              Posted by <b>Tyler Wittig</b> on 2020-09-01
+            </p>
+          </div>
+        </article>
+
+        <article>
+          <a class="img-link" href="#">
+            <img src="/img/projects/connect-four/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
+          </a>
+          <div class="details">
+            <ul class="tags">
+              <li><a href="#">Web Game</a></li>
+              <li><a href="#">JavaScript</a></li>
+              <li><a href="#">Grid Traversal</a></li>
+              <li><a href="#">Responsive</a></li>
+            </ul>
+            <h3>
+              <a href="#">
+                First Portfolio Website is First that is on the Web Never the
+                Last because it is First
+              </a>
+            </h3>
+            <p>
+              Serving as a playground for HTML, CSS, and JavaScript as I learned
+              more about web development, this static portfolio website underwent
+              numerous design changes and refactors in its time active...
+            </p>
+          </div>
+          <div class="author">
+            <img src="/img/profile.jpg"/>
+            <p>
+              Posted by <b>Tyler Wittig</b> on 2020-09-01
+            </p>
+          </div>
+        </article>
+
+        <article>
+          <a class="img-link" href="#">
+            <img src="/img/projects/connect-four/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
+          </a>
+          <div class="details">
+            <ul class="tags">
+              <li><a href="#">Web Game</a></li>
+              <li><a href="#">JavaScript</a></li>
+              <li><a href="#">Grid Traversal</a></li>
+              <li><a href="#">Responsive</a></li>
+            </ul>
+            <h3>
+              <a href="#">
+                First Portfolio Website is First that is on the Web Never the
+                Last because it is First
+              </a>
+            </h3>
+            <p>
+              Serving as a playground for HTML, CSS, and JavaScript as I learned
+              more about web development, this static portfolio website underwent
+              numerous design changes and refactors in its time active...
+            </p>
+          </div>
+          <div class="author">
+            <img src="/img/profile.jpg"/>
+            <p>
+              Posted by <b>Tyler Wittig</b> on 2020-09-01
+            </p>
+          </div>
+        </article>
+
+        <article>
+          <a class="img-link" href="#">
+            <img src="/img/projects/connect-four/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
+          </a>
+          <div class="details">
+            <ul class="tags">
+              <li><a href="#">Web Game</a></li>
+              <li><a href="#">JavaScript</a></li>
+              <li><a href="#">Grid Traversal</a></li>
+              <li><a href="#">Responsive</a></li>
+            </ul>
+            <h3>
+              <a href="#">
+                First Portfolio Website is First that is on the Web Never the
+                Last because it is First
+              </a>
+            </h3>
+            <p>
+              Serving as a playground for HTML, CSS, and JavaScript as I learned
+              more about web development, this static portfolio website underwent
+              numerous design changes and refactors in its time active...
+            </p>
+          </div>
+          <div class="author">
+            <img src="/img/profile.jpg"/>
+            <p>
+              Posted by <b>Tyler Wittig</b> on 2020-09-01
+            </p>
+          </div>
+        </article>
+
+        <article>
+          <a class="img-link" href="#">
+            <img src="/img/projects/connect-four/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
+          </a>
+          <div class="details">
+            <ul class="tags">
+              <li><a href="#">Web Game</a></li>
+              <li><a href="#">JavaScript</a></li>
+              <li><a href="#">Grid Traversal</a></li>
+              <li><a href="#">Responsive</a></li>
+            </ul>
+            <h3>
+              <a href="#">
+                First Portfolio Website is First that is on the Web Never the
+                Last because it is First
+              </a>
+            </h3>
+            <p>
+              Serving as a playground for HTML, CSS, and JavaScript as I learned
+              more about web development, this static portfolio website underwent
+              numerous design changes and refactors in its time active...
+            </p>
+          </div>
+          <div class="author">
+            <img src="/img/profile.jpg"/>
+            <p>
+              Posted by <b>Tyler Wittig</b> on 2020-09-01
+            </p>
+          </div>
+        </article>
+
+        <article>
+          <a class="img-link" href="#">
+            <img src="/img/projects/connect-four/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
+          </a>
+          <div class="details">
+            <ul class="tags">
+              <li><a href="#">Web Game</a></li>
+              <li><a href="#">JavaScript</a></li>
+              <li><a href="#">Grid Traversal</a></li>
+              <li><a href="#">Responsive</a></li>
+            </ul>
+            <h3>
+              <a href="#">
+                First Portfolio Website is First that is on the Web Never the
+                Last because it is First
+              </a>
+            </h3>
+            <p>
+              Serving as a playground for HTML, CSS, and JavaScript as I learned
+              more about web development, this static portfolio website underwent
+              numerous design changes and refactors in its time active...
+            </p>
+          </div>
+          <div class="author">
+            <img src="/img/profile.jpg"/>
+            <p>
+              Posted by <b>Tyler Wittig</b> on 2020-09-01
+            </p>
+          </div>
+        </article>
+
+        <article>
+          <a class="img-link" href="#">
+            <img src="/img/projects/connect-four/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
+          </a>
+          <div class="details">
+            <ul class="tags">
+              <li><a href="#">Web Game</a></li>
+              <li><a href="#">JavaScript</a></li>
+              <li><a href="#">Grid Traversal</a></li>
+              <li><a href="#">Responsive</a></li>
+            </ul>
+            <h3>
+              <a href="#">
+                First Portfolio Website is First that is on the Web Never the
+                Last because it is First
+              </a>
+            </h3>
+            <p>
+              Serving as a playground for HTML, CSS, and JavaScript as I learned
+              more about web development, this static portfolio website underwent
+              numerous design changes and refactors in its time active...
+            </p>
+          </div>
+          <div class="author">
+            <img src="/img/profile.jpg"/>
+            <p>
+              Posted by <b>Tyler Wittig</b> on 2020-09-01
+            </p>
+          </div>
+        </article>
+
+        <article>
+          <a class="img-link" href="#">
+            <img src="/img/projects/connect-four/title-card.png" loading="lazy" alt="First Portfolio Title Card" />
+          </a>
+          <div class="details">
+            <ul class="tags">
+              <li><a href="#">Web Game</a></li>
+              <li><a href="#">JavaScript</a></li>
+              <li><a href="#">Grid Traversal</a></li>
+              <li><a href="#">Responsive</a></li>
+            </ul>
+            <h3>
+              <a href="#">
+                First Portfolio Website is First that is on the Web Never the
+                Last because it is First
+              </a>
+            </h3>
+            <p>
+              Serving as a playground for HTML, CSS, and JavaScript as I learned
+              more about web development, this static portfolio website underwent
+              numerous design changes and refactors in its time active...
+            </p>
+          </div>
+          <div class="author">
+            <img src="/img/profile.jpg"/>
+            <p>
+              Posted by <b>Tyler Wittig</b> on 2020-09-01
+            </p>
+          </div>
+        </article>
+
+      </div>  <!-- ./grid -->
+
+      <div class="page-links-wrap">
+        <nav class="page-links">
+          <a class="end-link" href="#">Previous</a>
+          <span class="active">1</span>
+          <a href="#">2</a>
+          <a href="#">3</a>
+          <span>...</span>
+          <a href="#">10</a>
+          <a class="end-link" href="#">Next</a>
+        </nav>
+      </div>
 
     </section>
 
