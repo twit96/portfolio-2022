cfaffa79e04a2805a90a1827c982be2cec965f04
Tyler <44188028+twit96@users.noreply.github.com>
2021-11-06 19:43:48 -0500

    console.log test statement

--- includes/views/articles.php
+++ includes/views/articles.php
@@ -5,6 +5,7 @@
 error_reporting(E_ALL);
 ini_set("display_errors", "on");
 
+echo '<script>console.log('.__DIR__.'/../controllers/articles_includes.php'.')</script>';
 include (__DIR__.'/../controllers/articles_includes.php');
 
 $blog_posts = $getBlogPosts();
