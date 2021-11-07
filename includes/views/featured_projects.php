bdaebf13b6c080ec970099610086fc2facb1190f
Tyler <44188028+twit96@users.noreply.github.com>
2021-11-07 17:15:45 -0600

    Update featured_projects.php
    removed all code from inside of old function that is no longer called (was never executed in last commit as a result of being left inside)

--- includes/views/featured_projects.php
+++ includes/views/featured_projects.php
@@ -6,67 +6,65 @@ error_reporting(E_ALL);
 ini_set("display_errors", "on");
 
 
-function displayFeatured($mysqli) {
-  // Opening HTML
-  echo <<<TOP
-  <article id="featured">
-        <div class="flex">
-          <div class="heading-card">
-            <h2>Featured</h2>
-          </div>
-          <div class="card-container">
-  TOP;
+// Opening HTML
+echo <<<TOP
+<article id="featured">
+      <div class="flex">
+        <div class="heading-card">
+          <h2>Featured</h2>
+        </div>
+        <div class="card-container">
+TOP;
 
-  include (__DIR__ .'/../controllers/projects_includes.php');
+include (__DIR__ .'/../controllers/projects_includes.php');
 
 
-  $featured_projects = getProjects($mysqli, TRUE);
-  foreach ($featured_projects as $project) {
+$featured_projects = getProjects($mysqli, TRUE);
+foreach ($featured_projects as $project) {
 
-    // config primary link
-    $primary_link_url = null;
-    $primary_link_text = null;
-    if (is_array($project->primary_link) && sizeof($project->primary_link) != 0) {
-      $primary_link_url = reset($project->primary_link);
-      $primary_link_text = key($project->primary_link);
-    }
-
-    echo '<div class="card">';
-    echo '<div class="featured-badge">';
-    echo '<span>#'.$project->featured.'</span>';
-    echo '</div>';
-    echo '<img src="/img/projects/'.$project->directory.'/'.$project->image.'" loading="lazy" alt="'.$project->title.' Title Card" />';
-    echo '<em>'.$project->blurb.'</em>';
-    echo '<div class="info">';
-    echo '<div class="btn-text date">';
-    echo '<span class="icon"></span>';
-    echo '<span>'.$project->date.'</span>';
-    echo '</div>';
-    if ($primary_link_url != null) {
-      echo '<a class="btn-text link" href="'.$primary_link_url.'">';
-      echo '<span class="icon"></span>'.$primary_link_text;
-      echo '</a>';
-    }
-    echo '</div>';  // ./info
-    echo '</div>';  // ./card
-
+  // config primary link
+  $primary_link_url = null;
+  $primary_link_text = null;
+  if (is_array($project->primary_link) && sizeof($project->primary_link) != 0) {
+    $primary_link_url = reset($project->primary_link);
+    $primary_link_text = key($project->primary_link);
   }
 
+  echo '<div class="card">';
+  echo '<div class="featured-badge">';
+  echo '<span>#'.$project->featured.'</span>';
+  echo '</div>';
+  echo '<img src="/img/projects/'.$project->directory.'/'.$project->image.'" loading="lazy" alt="'.$project->title.' Title Card" />';
+  echo '<em>'.$project->blurb.'</em>';
+  echo '<div class="info">';
+  echo '<div class="btn-text date">';
+  echo '<span class="icon"></span>';
+  echo '<span>'.$project->date.'</span>';
+  echo '</div>';
+  if ($primary_link_url != null) {
+    echo '<a class="btn-text link" href="'.$primary_link_url.'">';
+    echo '<span class="icon"></span>'.$primary_link_text;
+    echo '</a>';
+  }
+  echo '</div>';  // ./info
+  echo '</div>';  // ./card
 
-  // Closing HTML
-  echo <<<BOTTOM
-  \n          <a class="card link" href="/projects">
-              <span>View All <br />Projects</span>
-            </a>
-          </div>  <!-- ./card-container -->
-        </div>  <!-- ./flex -->
-        <p>
-          To view all of my work, please visit my
-          <a href="/projects">projects page</a>.
-        </p>
-      </article>\n
-  BOTTOM;
 }
+
+
+// Closing HTML
+echo <<<BOTTOM
+\n          <a class="card link" href="/projects">
+            <span>View All <br />Projects</span>
+          </a>
+        </div>  <!-- ./card-container -->
+      </div>  <!-- ./flex -->
+      <p>
+        To view all of my work, please visit my
+        <a href="/projects">projects page</a>.
+      </p>
+    </article>\n
+BOTTOM;
 
 
 ?>
