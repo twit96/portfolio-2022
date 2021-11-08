03b1d89ceb547bfe951a50e914bd09ba8c3e75ef
Tyler <44188028+twit96@users.noreply.github.com>
2021-11-07 20:05:49 -0600

    Move post into its own page

---
+++ includes/views/post.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+* Display all errors.
+*/
+error_reporting(E_ALL);
+ini_set("display_errors", "on");
+
+
+include (__DIR__.'/../controllers/articles_includes.php');
+
+
+if (isset($_GET['title'])) {
+  $title = ucfirst(join(" ", explode("-", $_GET["title"])));
+  $post = getBlogPosts($mysqli, $title, null);
+  if (sizeof($post) > 0) {
+    $post = $post[0];
+  } else {
+    // no blog post of the given name - redirect to articles page
+    header('Location: ./articles');
+    die();
+  }
+} else {
+  // no blog post selected - redirect to articles page
+  header('Location: ./articles');
+  die();
+}
+
+
+// Opening HTML
+echo <<<TOP
+\n    <main id="post">
+      <div class="wrapper">
+TOP;
+
+echo '<article>';
+if (!empty($post->image)) {
+  echo '<div class="card details" style="background-image: url(/img/articles/'.$post->directory.'/'.$post->image.')">';
+} else {
+  echo '<div class="card details">';
+}
+echo '<h1>'.$post->title.'</h1>';
+if (is_array($post->tags) && sizeof($post->tags) > 0) {
+  echo '<ul class="tags">';
+  foreach ($post->tags as $tag) {
+    echo '<li><a href="#">'.$tag.'</a></li>';
+  }
+  echo '</ul>';
+}
+echo '<div class="author">';
+echo '<img src="/img/profile.jpg" alt="'.$post->author.' Image" />';
+echo '<p><b>'.$post->author.'</b> on <span>'.$post->date_posted.'</span> ';
+if ($post->date_posted != $post->date_updated) {
+  echo '(Updated on <span>'.$post->date_updated.')</span>';
+}
+echo '</p>';
+echo '</div>';  //./author
+echo '<div class="btm-border"></div>';
+echo '</div>';  // ./card details
+echo '<div class="card">'.$post->post.'</div>';  // post content
+echo '</article>';
+
+echo '<div id="sidebar">';
+include (__DIR__.'/../templates/toc.html');
+echo '</div>';
+
+// Closing HTML
+echo <<<BTM
+\n    </div>  <!-- ./wrapper -->
+    </main>
+BTM;
+
+
+?>
