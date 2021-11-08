a7565b5ceb493306b095503bfd525475de349180
Tyler <44188028+twit96@users.noreply.github.com>
2021-11-08 14:22:40 -0600

    Add Tag Filtering to articles page

--- articles.php
+++ articles.php
@@ -12,6 +12,8 @@ include ('./includes/controllers/articles_includes.php');
 
 if (isset($_GET['post'])) {
   displayOneArticle($mysqli);
+} else if (isset($_GET['tag'])) {
+  displayTaggedArticles($mysqli);
 } else {
   displayAllArticles($mysqli);
 }
@@ -91,6 +93,94 @@ function displayOneArticle($mysqli) {
 }
 
 
+function displayTaggedArticles($mysqli) {
+  DEFINE("FILENAME", 'articles');
+  include('./includes/templates/head.php');
+  echo <<<HEAD_END
+  \n  </head>
+    <body>
+  HEAD_END;
+
+  include('./includes/templates/header.php');
+
+  // Opening HTML
+  echo <<<TOP
+  \n    <main id="articles">
+        <div class="wrapper">
+          <h1>Articles</h1>
+        </div>
+        <div class="wrapper grid">
+  TOP;
+
+  $tag = str_replace(" ", "-", strtolower($_GET['tag']));
+  $blog_posts = getBlogPosts($mysqli, null, $tag);
+  foreach ($blog_posts as $post) {
+    $this_link = '?post='.str_replace(" ", "-", strtolower($post->title));
+
+    echo '<article>';
+
+    if (!empty($post->directory) && (!empty($post->image))) {
+      echo <<<IMG_LINK
+      <a class="img-link" href="{$this_link}">
+        <img src="{$post->path}/{$post->image}" loading="lazy" alt="{$post->title} Title Card" />
+      </a>
+      IMG_LINK;
+    }
+
+    // Details
+    echo '<div class="details">';
+    // tags
+    if (is_array($post->tags) && sizeof($post->tags) > 0) {
+      echo '<ul class="tags">';
+      foreach ($post->tags as $tag) {
+        echo '<li><a href=?tag='.str_replace(" ", "-", strtolower($tag)).'>'.$tag.'</a></li>';
+      }
+      echo '</ul>';
+    }
+    // title
+    echo '<h3><a href="'.$this_link.'">'.$post->title.'</a></h3>';
+    // blurb
+    echo '<p>';
+    if (strlen($post->post) > 150) {
+      $blurb = substr($post->post,0,150);
+      // replace any headings in blurb
+      for ($i=1; $i<7; $i++) {
+        $blurb = str_replace('h'.$i, 'b style="display:block;"', $blurb);
+      }
+      echo $blurb.'...';
+    } else {
+      echo $post->post;
+    }
+    echo '</p>';
+    echo '</div>';
+    // Author
+    echo '<div class="author">';
+    echo '<img src="'.$post->author_img_path.'" alt="'.$post->author.' Image" />';
+    echo '<p><b>'.$post->author.'</b> on <span>'.$post->date_posted.'</span> ';
+    if ($post->date_posted != $post->date_updated) {
+      echo '(Updated on <span>'.$post->date_updated.')</span>';
+    }
+    echo'</p>';
+    echo '</div>';
+    echo '</article>';
+  }
+
+  // Closing HTML
+  echo <<<BTM
+  \n    </div>  <!-- ./wrapper grid -->
+      </main>
+  BTM;
+
+  include('./includes/templates/footer.php');
+
+  echo <<<PAGE_END
+    </body>
+  </html>
+  PAGE_END;
+
+}
+
+
 function displayAllArticles($mysqli) {
 
   DEFINE("FILENAME", 'articles');
@@ -111,7 +201,7 @@ function displayAllArticles($mysqli) {
         <div class="wrapper grid">
   TOP;
 
-  $blog_posts = getBlogPosts($mysqli);
+  $blog_posts = getBlogPosts($mysqli, null, null);
   foreach ($blog_posts as $post) {
     $this_link = '?post='.str_replace(" ", "-", strtolower($post->title));
 
