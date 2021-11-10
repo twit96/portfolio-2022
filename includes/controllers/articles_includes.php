855ffe26530be87ba5e81187a3d65bd88b47bcc1
Tyler <44188028+twit96@users.noreply.github.com>
2021-11-10 14:28:21 -0600

    Update articles_includes.php

--- includes/controllers/articles_includes.php
+++ includes/controllers/articles_includes.php
@@ -21,16 +21,17 @@ $mysqli->select_db($dbName) or die($mysqli->error);
 
 
 function getBlogPosts($mysqli, $in_title=null, $in_tag_name=null) {
+
   if (!empty($in_title)) {
     $in_title = $mysqli->real_escape_string($in_title);
     $command = "SELECT * FROM blog_posts WHERE title='".$in_title."' ORDER BY date_posted DESC;";
   } else if (!empty($in_tag_name)) {
     $in_tag_name = $mysqli->real_escape_string($in_tag_name);
-    // $command = "SELECT blog_posts.* FROM blog_post_tags LEFT JOIN (blog_posts) ON (blog_post_tags.blog_post_id = blog_posts.id) WHERE blog_post_tags.tag_id IN (SELECT id FROM tags WHERE name='".$in_tag_name."') ORDER BY blog_posts.date_posted DESC;";
     $command = "SELECT p.* FROM blog_posts AS p JOIN blog_post_tags AS bpt ON p.id = bpt.blog_post_id JOIN tags AS t ON t.id = bpt.tag_id WHERE t.name = '".$in_tag_name."' ORDER BY date_posted DESC;";
   } else {
     $command = "SELECT * FROM blog_posts ORDER BY date_posted DESC;";
   }
+
   $result = $mysqli->query($command);
   if (!$result) { die('Query failed: '.$mysqli->error.'<br>'); }
 
@@ -49,6 +50,7 @@ function getBlogPosts($mysqli, $in_title=null, $in_tag_name=null) {
     );
     array_push($post_array, $this_post);
   }
+
   return $post_array;
 }
 
