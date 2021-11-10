0654ade8859894ea05b396cd444efdc28d990e19
Tyler <44188028+twit96@users.noreply.github.com>
2021-11-10 14:39:13 -0600

    Update articles_includes.php

--- includes/controllers/articles_includes.php
+++ includes/controllers/articles_includes.php
@@ -26,7 +26,7 @@ function getBlogPosts($mysqli, $in_title=null, $in_tag_name=null) {
     $in_title = $mysqli->real_escape_string($in_title);
     $command = "SELECT * FROM blog_posts WHERE title='".$in_title."' ORDER BY date_posted DESC;";
   } else if (!empty($in_tag_name)) {
-    $in_tag_name = $mysqli->real_escape_string($in_tag_name);
+    $in_tag_name = $mysqli->real_escape_string(str_replace("-", " ", $in_tag_name));
     $command = "SELECT p.* FROM blog_posts AS p JOIN blog_post_tags AS bpt ON p.id = bpt.blog_post_id JOIN tags AS t ON t.id = bpt.tag_id WHERE t.name = '".$in_tag_name."' ORDER BY date_posted DESC;";
   } else {
     $command = "SELECT * FROM blog_posts ORDER BY date_posted DESC;";
