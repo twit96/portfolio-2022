b4bf3826c7f0cd00fbaa40f5be3839c4937c3874
Tyler <44188028+twit96@users.noreply.github.com>
2021-11-10 14:23:40 -0600

    Simplified Tag Query - Real Escaped Inputs

--- includes/controllers/articles_includes.php
+++ includes/controllers/articles_includes.php
@@ -22,10 +22,12 @@ $mysqli->select_db($dbName) or die($mysqli->error);
 
 function getBlogPosts($mysqli, $in_title=null, $in_tag_name=null) {
   if (!empty($in_title)) {
+    $in_title = $mysqli->real_escape_string($in_title);
     $command = "SELECT * FROM blog_posts WHERE title='".$in_title."' ORDER BY date_posted DESC;";
   } else if (!empty($in_tag_name)) {
-    // $command = "SELECT blog_posts.* FROM blog_post_tags LEFT JOIN (blog_posts) ON (blog_post_tags.blog_post_id = blog_posts.id) WHERE blog_post_tags.tag_name='".$in_tag_name."' ORDER BY blog_posts.date_posted DESC;";
-    $command = "SELECT blog_posts.* FROM blog_post_tags LEFT JOIN (blog_posts) ON (blog_post_tags.blog_post_id = blog_posts.id) WHERE blog_post_tags.tag_id IN (SELECT id FROM tags WHERE name='".$in_tag_name."') ORDER BY blog_posts.date_posted DESC;";
+    $in_tag_name = $mysqli->real_escape_string($in_tag_name);
+    // $command = "SELECT blog_posts.* FROM blog_post_tags LEFT JOIN (blog_posts) ON (blog_post_tags.blog_post_id = blog_posts.id) WHERE blog_post_tags.tag_id IN (SELECT id FROM tags WHERE name='".$in_tag_name."') ORDER BY blog_posts.date_posted DESC;";
+    $command = "SELECT p.* FROM blog_posts AS p JOIN blog_post_tags AS bpt ON p.id = bpt.blog_post_id JOIN tags AS t ON t.id = bpt.tag_id WHERE t.name = '".$in_tag_name."' ORDER BY date_posted DESC;"
   } else {
     $command = "SELECT * FROM blog_posts ORDER BY date_posted DESC;";
   }
