b255f6764e6452a0939ed1383efb8ccf84651dd6
Tyler <44188028+twit96@users.noreply.github.com>
2021-11-10 20:31:33 -0600

    Renamed 'includes/models/' directory files
    Renamed 'projects_includes.php' and 'articles_includes.php' to 'projects.php' and 'articles.php'

--- articles.php
+++ articles.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", "on");
 
 
-include ('./includes/models/articles_includes.php');
+include ('./includes/models/articles.php');
 
 
 if (isset($_GET['post'])) {
