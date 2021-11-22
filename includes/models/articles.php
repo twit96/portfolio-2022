c59ff88d39ef53d03bcb110c0b197daacf4efed9
Tyler <44188028+twit96@users.noreply.github.com>
2021-11-21 21:25:55 -0600

    fix articles page prepared statements bind params
    accidentally set bind_param to i when it shouldve been an s (string not integer - since refers to title/tag)

--- includes/models/articles.php
+++ includes/models/articles.php
@@ -8,12 +8,12 @@ function getBlogPosts($mysqli, $in_title=null, $in_tag_name=null) {
 
   if (!empty($in_title)) {
     $stmt = $mysqli->prepare("SELECT * FROM blog_posts WHERE title=? ORDER BY date_posted DESC");
-    $stmt->bind_param("i", $this_title);
+    $stmt->bind_param("s", $this_title);
     $this_title = $in_title;
 
   } else if (!empty($in_tag_name)) {
     $stmt = $mysqli->prepare("SELECT p.* FROM blog_posts AS p JOIN blog_post_tags AS bpt ON p.id = bpt.blog_post_id JOIN tags AS t ON t.id = bpt.tag_id WHERE t.name=? ORDER BY date_posted DESC");
-    $stmt->bind_param("i", $this_tag);
+    $stmt->bind_param("s", $this_tag);
     $this_tag = $in_tag_name;
 
   } else {
