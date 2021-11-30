760bf799fc0e6fd3c5efc3af02a630cefe14027e
Tyler <44188028+twit96@users.noreply.github.com>
2021-11-30 14:05:20 -0600

    implement prepared statements function on articles model

--- includes/models/articles.php
+++ includes/models/articles.php
@@ -7,26 +7,29 @@ include (__DIR__ .'/../classes/BlogPost.php');
 function getBlogPosts($mysqli, $in_title=null, $in_tag_name=null) {
 
   if (!empty($in_title)) {
-    $stmt = $mysqli->prepare("SELECT * FROM blog_posts WHERE title=? ORDER BY date_posted DESC");
-    $stmt->bind_param("s", $this_title);
-    $this_title = $in_title;
+    $result = getResults(
+      $mysqli,
+      "SELECT * FROM blog_posts WHERE title=? ORDER BY date_posted DESC",
+      "s",
+      array($in_title)
+    );
 
   } else if (!empty($in_tag_name)) {
-    $stmt = $mysqli->prepare("SELECT p.* FROM blog_posts AS p JOIN blog_post_tags AS bpt ON p.id = bpt.blog_post_id JOIN tags AS t ON t.id = bpt.tag_id WHERE t.name=? ORDER BY date_posted DESC");
-    $stmt->bind_param("s", $this_tag);
-    $this_tag = $in_tag_name;
+    $result = getResults(
+      $mysqli,
+      "SELECT p.* FROM blog_posts AS p JOIN blog_post_tags AS bpt ON p.id = bpt.blog_post_id JOIN tags AS t ON t.id = bpt.tag_id WHERE t.name=? ORDER BY date_posted DESC",
+      "s",
+      array($in_tag_name)
+    );
 
   } else {
-    $stmt = $mysqli->prepare("SELECT * FROM blog_posts ORDER BY date_posted DESC");
+    $result = getResults(
+      $mysqli,
+      "SELECT * FROM blog_posts ORDER BY date_posted DESC",
+      "",
+      array()
+    );
   }
-
-  $stmt->execute();
-  if ($stmt === false) {
-    error_log('mysqli execute() failed: ');
-    error_log( print_r( htmlspecialchars($stmt->error), true ) );
-  }
-  $result = $stmt->get_result();
-  $stmt->close();
 
   $post_array = array();
   while ($row = $result->fetch_assoc()) {
