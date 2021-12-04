897231bafbd1dc754d0ca17d59de637088cbdb2f
Tyler <44188028+twit96@users.noreply.github.com>
2021-12-03 22:19:57 -0600

    ServerDirectory calling own methods syntax fix
    "$this->" added where needed

--- includes/classes/ServerDirectory.php
+++ includes/classes/ServerDirectory.php
@@ -37,7 +37,7 @@ class ServerDirectory {
         mkdir($dst);
         $files = scandir($src);
         foreach ($files as $file)
-        if ($file != "." && $file != "..") mvdir("$src/$file", "$dst/$file");
+        if ($file != "." && $file != "..") $this->mvdir("$src/$file", "$dst/$file");
         rmdir($src);
       // files (recursive endpoints - rename copies then deletes)
       } else if (file_exists($src)) rename($src, $dst);
@@ -52,7 +52,7 @@ class ServerDirectory {
     if (is_dir($dir)) {
       $files = scandir($dir);
       foreach ($files as $file)
-      if ($file != "." && $file != "..") rrmdir("$dir/$file");
+      if ($file != "." && $file != "..") $this->rrmdir("$dir/$file");
       rmdir($dir);
     } else if (file_exists($dir)) unlink($dir);
   }
@@ -67,7 +67,7 @@ class ServerDirectory {
         mkdir($dst);
         $files = scandir($src);
         foreach ($files as $file)
-        if ($file != "." && $file != "..") rcopy("$src/$file", "$dst/$file");
+        if ($file != "." && $file != "..") $this->rcopy("$src/$file", "$dst/$file");
       }
       else if (file_exists($src)) copy($src, $dst);
     }
