6ccbf9ac65938c104a7addfff0c1d9a319021d1a
Tyler <44188028+twit96@users.noreply.github.com>
2021-12-04 17:10:35 -0600

    Homepage Intro and Demo moved into Separate Files in "includes/views/"

--- index.php
+++ index.php
@@ -9,101 +9,26 @@ HEAD_END;
 
 include('./includes/templates/header.php');
 
-?>
-
-    <main id="home">
+// Opening HTML
+echo <<<TOP
+\n    <main id="home">
       <div class="wrapper">
+TOP;
 
-        <article id="intro">
-          <div id="logo-container">
-    				<div id="main-logo-wrap">
-    					<img id="main-logo" src="/img/icon.png" alt="site logo" />
-    					<span class="main-logo-overlay"></span>
-    				</div>
-    			</div>
-          <div class="blurb">
-            <p>
-              Well <i>hello</i> world,
-              <br />
-              My name is Tyler and I am a full-stack web developer.<br />
-              I specialize in...
-            </p>
-            <p>
-              <span class="active tagline">Custom Websites</span>
-              <span class="tagline">Website Redesigns</span>
-              <span class="tagline">Game Development</span>
-            </p>
-          </div>
-          <div class="details">
-            <h1>Tyler Wittig</h1>
-            <h2>Web Developer</h2>
-            <div class="links">
-              <a href="mailto:tylerwittig@utexas.edu?subject=Web%20Developer%20Information" target="_blank" rel="noreferrer">
-                <img src="/img/logo_email.png" alt="twitter logo" />
-              </a>
-              <a href="https://github.com/twit96" target="_blank" rel="noreferrer">
-                <img src="/img/logo_github.png" alt="github logo" />
-              </a>
-              <a href="https://www.linkedin.com/in/tylerwittig/" target="_blank" rel="noreferrer">
-                <img src="/img/logo_linkedin.png" alt="linkedin logo" />
-              </a>
-            </div>  <!-- ./links -->
-          </div>  <!-- ./details -->
-        </article>  <!-- #/intro -->
+include('./includes/views/intro.html');
+include('./includes/views/skills.html');
+include('./includes/views/projects_featured.php');
+include('./includes/views/demo.html');
 
-        <?php include('./includes/views/skills.html'); ?>
-        <?php include('./includes/views/projects_featured.php'); ?>
+// Closing HTML
+echo "\n".'      </div>';
+echo "\n".'    </main>'."\n\n";
 
-        <article id="demo">
-          <div class="flex">
-            <div class="blurb">
-              <h2>Your New Website</h2>
-              <p>
-                Whether a static website or one with an integrated database,
-                I build websites from the ground up to meet your needs.
-              </p>
-            </div>
-            <figure class="with-css with-js with-php with-sql with-host">
-              <div class="website">
-                <div class="monitor">
-                  <span class="title">Your Website</span>
-                </div>
-                <span class="text">HTML</span>
-                <span class="text"> / CSS</span>
-                <span class="text"> / JS</span>
-              </div>
-              <div class="pipeline">
-                <span class="pipe"></span>
-                <span class="pipe"></span>
-                <span class="text">PHP</span>
-              </div>
-              <div class="database">
-                <div>
-                  <span class="light"></span>
-                  <span class="light"></span>
-                  <span class="light"></span>
-                  <span class="title">Database</span>
-                </div>
-                <span class="text">SQL</span>
-              </div>
-              <span class="text">Host</span>
-            </figure>
-          </div>  <!-- ./flex -->
-          <div id="demo-controls">
-            <div class="btn-text play">
-              <span class="icon"></span>
-              <span class="text">Play</span>
-            </div>
-            <div class="btn-text stop disabled">
-              <span class="icon"></span>
-              <span class="text">Stop</span>
-            </div>
-          </div>  <!-- #/demo-controls -->
-        </article>  <!-- #/demo -->
+include('./includes/templates/footer.php');
 
-      </div>  <!-- ./wrapper -->
-    </main>  <!-- #home -->
-
-<?php include('./includes/templates/footer.php'); ?>
+echo <<<PAGE_END
   </body>
 </html>
+PAGE_END;
+
+?>
