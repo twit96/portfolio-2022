a7f5126a13eb209532eb4efa46e37cb6a105e5e3
Tyler <44188028+twit96@users.noreply.github.com>
2021-12-04 10:45:37 -0600

    Revert "Test - tweak Tags for loop on articles page"
    This reverts commit 72722bebcba98d0d0a8ef5971c13f18374a99b57.

--- articles.php
+++ articles.php
@@ -58,9 +58,8 @@ function displayOneArticle($mysqli) {
   echo '<h1>'.$post->title.'</h1>';
   if (is_array($post->tags) && sizeof($post->tags) > 0) {
     echo '<ul class="tags">';
-    for ($i=0; $i<count($post->tags); $i++) {
-      $this_tag = $post->tags[$i];
-      echo '<li><a href="../tag/'.str_replace(" ", "-", $this_tag->name).'">'.$this_tag->name.'</a></li>';
+    foreach ($post->tags as $tag) {
+      echo '<li><a href="../tag/'.str_replace(" ", "-", $tag->name).'">'.$tag->name.'</a></li>';
     }
     echo '</ul>';
   }
