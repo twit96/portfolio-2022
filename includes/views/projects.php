086cb8a3f2efd216874d50d2a5c9a0a3c700ce5a
Tyler <44188028+twit96@users.noreply.github.com>
2021-12-04 17:13:02 -0600

    Projects Page content moved into its own "includes/views" file

---
+++ includes/views/projects.php
@@ -0,0 +1,68 @@
+<?php
+
+
+include(__DIR__ .'/../models/projects.php');
+
+
+$projects = getProjects($mysqli, FALSE);
+foreach ($projects as $project) {
+
+  // config primary link
+  $primary_link_url = null;
+  $primary_link_text = null;
+  if (!empty($project->primary_link)) {
+    $primary_link = $project->primary_link;
+    $primary_link_url = $primary_link->url;
+    $primary_link_text = $primary_link->text;
+  }
+
+  // display project info
+  echo '<article>';
+  echo '<figure>';
+  if ($project->featured > 0) {
+    echo '<div class="featured-badge">';
+    echo '<span>#'.$project->featured.'</span>';
+    echo '</div>';
+  }
+  // Figure
+  if ($primary_link_url != null) {
+    echo '<a href="'.$primary_link_url.'">';
+    echo '<img src="'.$project->image->path.$project->image->name.'" loading="lazy" alt="'.$project->title.' Title Card" />';
+    echo '</a>';
+  } else {
+    echo '<img src="'.$project->image->path.$project->image->name.'" loading="lazy" alt="'.$project->title.' Title Card" />';
+  }
+  echo '<figcaption>'.$project->blurb.'</figcaption>';
+  echo '</figure>';
+  // Details
+  echo '<div class="details">';
+  // heading
+  if ($primary_link_url != null) {
+    echo '<h3><a href="'.$primary_link_url.'">'.$project->title.'</a></h3>';
+  } else {
+    echo '<h3>'.$project->title.'</h3>';
+  }
+  // date/description
+  echo '<em>'.$project->date.'</em>';
+  echo '<p>'.$project->description.'</p>';
+  // links
+  if ($primary_link_url != null) {
+    // primary link
+    echo '<a class="btn-text link" href="'.$primary_link_url.'">';
+    echo '<span class="icon"></span>'.$primary_link_text;
+    echo '</a>';
+    // other links
+    if (is_array($project->other_links) && sizeof($project->other_links) > 0) {
+      foreach ($project->other_links as $link) {
+        echo '<a class="btn-text link" href="'.$link->url.'">';
+        echo '<span class="icon"></span>'.$link->text;
+        echo '</a>';
+      }
+    }
+  }
+  echo '</div>';
+  echo '</article>';
+}
+
+
+?>
