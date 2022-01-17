22471eb41ea529cd502df0e7407b5e2a422082b3
Tyler <44188028+twit96@users.noreply.github.com>
2022-01-17 16:15:01 -0600

    Test2 - leave bold tags alone

--- articles.php
+++ articles.php
@@ -147,7 +147,13 @@ function displayTaggedArticles($mysqli) {
     // blurb
     echo '<p>';
     if (strlen($post->post) > 150) {
-      $blurb = strip_tags(substr($post->post,0,150));
+      $blurb = substr($post->post,0,150);
+      // replace any headings in blurb
+      for ($i=1; $i<7; $i++) {
+        $blurb = str_replace('h'.$i, 'b style="display:block;"', $blurb);
+      }
+      // remove all html chars except bold tags
+      $blurb = strip_tags($blurb, "<b>");
       echo $blurb.'...';
     } else {
       echo $post->post;
