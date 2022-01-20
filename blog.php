80b44cb0a0f40a2456d405e54b8ae2cd65a0eb65
Tyler <44188028+twit96@users.noreply.github.com>
2022-01-20 12:06:50 -0600

    Update blog.php

--- blog.php
+++ blog.php
@@ -283,12 +283,20 @@ function displayAllPosts($mysqli) {
     echo '</div>';
     echo '</article>';
   }
+  // Close BlogPost Grid Section
+  echo '</div>  <!-- ./wrapper grid -->';
+
+
+  // Page Indicator Section
+  $total_posts = getNumBlogPosts($mysqli);
+  $total_pages = ceil($total_posts/12);
+  $total_pages = $total_posts;
+  echo '<script>alert("$total_pages: '.$total_pages.'");</script>';
+  include ('./includes/templates/PageIndicator.php');
+  
 
   // Closing HTML
-  echo <<<BTM
-  \n    </div>  <!-- ./wrapper grid -->
-      </main>
-  BTM;
+  echo '</main>';
 
   include('./includes/templates/footer.php');
 
