ed0fcb89839bf3f9aa95fd04a3f371b9fce1bea4
Tyler <44188028+twit96@users.noreply.github.com>
2022-01-20 11:36:34 -0600

    add error reporting to all files

--- includes/helpers/db_connect.php
+++ includes/helpers/db_connect.php
@@ -1,6 +1,13 @@
 <?php
 
 
+/**
+* Display all errors.
+*/
+error_reporting(E_ALL);
+ini_set("display_errors", "on");
+
+
 // Connect to MySQL Server
 $server = "localhost";
 $user   = "portfolio_user";
