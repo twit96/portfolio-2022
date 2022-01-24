0bc5a843e5cbcf66bd4f774992a6ae21fec3bdf0
Tyler <44188028+twit96@users.noreply.github.com>
2022-01-24 16:38:34 -0600

    change admin page includes to require_once

--- public/admin.php
+++ public/admin.php
@@ -1,27 +1,27 @@
 <?php
 
 DEFINE("FILENAME", 'admin');
-include('../src/View/common/head.php');
+require_once (__DIR__ .'/../src/View/common/head.php');
 echo <<<HEAD_END
 \n  </head>
   <body>
 HEAD_END;
 
-include('../src/View/common/header.php');
+require_once (__DIR__ .'/../src/View/common/header.php');
 echo <<<SECTION_TOP
 <main id="admin">
   <div class="wrapper">
     <h1>Admin</h1>
 SECTION_TOP;
 
-include('../src/Controller/admin.php');
+require_once (__DIR__ .'/../src/Controller/admin.php');
 
 echo <<<SECTION_BTM
   </div>
 </main>
 SECTION_BTM;
 
-include('../src/View/common/footer.php');
+require_once (__DIR__ .'/../src/View/common/footer.php');
 
 
 echo <<<PAGE_END
