87d009410303515918a8f068c71c193a87b03943
Tyler <44188028+twit96@users.noreply.github.com>
2022-01-24 14:01:58 -0600

    Create Dedicated Config File
    Created "config.ini.php" in the "config" directory.
    Pull database information from this new file in "db_connect.php".

---
+++ config/config.ini.php
@@ -0,0 +1,9 @@
+;<?php die(); ?>
+; To prevent accidental public access, file uses php extension and die command.
+; These lines will be ignored when read as a .ini file, since they are comments.
+
+[database]
+db_server   = localhost
+db_user     = portfolio_user
+db_password = portfolio_user_pass
+db_name     = Portfolio
--- config/db_connect.php
+++ config/db_connect.php
@@ -1,15 +1,20 @@
 <?php
 
 
-$server   = "localhost";
-$user     = "portfolio_user";
-$password = "portfolio_user_pass";
-$db_name  = "Portfolio";
+// Pull Database Info from Config File
+$ini = parse_ini_file(__DIR__ .'/config.ini.php', true)['database'];
 
-// Build Database Connection Object
+$server   = $ini["db_server"];
+$user     = $ini["db_user"];
+$password = $ini["db_password"];
+$db_name  = $ini["db_name"];
+
+// Establish Database Connection
 require_once(__DIR__ .'/../src/Model/DB.php');
 $db = new DB($server, $user, $password, $db_name);
-unset($server, $user, $password, $db_name);
 
+// Remove Sensitive Information from Memory
+$ini = $server = $user = $password = $db_name = null;
+unset($ini, $server, $user, $password, $db_name);
 
 ?>
