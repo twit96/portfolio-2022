262b76c9d806be13094488fcf10b48ce2ea4507e
Tyler <44188028+twit96@users.noreply.github.com>
2022-01-24 16:44:43 -0600

    change projects page includes to require_once
    and use __DIR__ in pathnames

--- public/projects.php
+++ public/projects.php
@@ -2,13 +2,13 @@
 
 
 DEFINE("FILENAME", 'projects');
-include('../src/View/common/head.php');
+require_once (__DIR__ .'/../src/View/common/head.php');
 echo <<<HEAD_END
 \n  </head>
   <body>
 HEAD_END;
 
-include('../src/View/common/header.php');
+require_once (__DIR__ .'/../src/View/common/header.php');
 
 
 // Opening HTML
@@ -18,13 +18,13 @@ echo <<<TOP
         <h1>Projects</h1>
 TOP;
 
-include('../src/View/projects/projects.php');
+require_once (__DIR__ .'/../src/View/projects/projects.php');
 
 // Closing HTML
 echo "\n".'      </div>';
 echo "\n".'    </main>'."\n\n";
 
-include('../src/View/common/footer.php');
+require_once (__DIR__ .'/../src/View/common/footer.php');
 
 echo <<<PAGE_END
   </body>
