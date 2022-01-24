e56381eeab5630b2e9d96b3e36a1b60addefc503
Tyler <44188028+twit96@users.noreply.github.com>
2022-01-24 16:40:05 -0600

    Update blog page require_once pathnames
    all now start with __DIR__

--- public/blog.php
+++ public/blog.php
@@ -7,15 +7,15 @@ error_reporting(E_ALL);
 ini_set("display_errors", "on");
 
 
-require_once ('../src/Controller/blog.php');
+require_once (__DIR__ .'/../src/Controller/blog.php');
 
 
 if (isset($_GET["id"])) {
-  require_once ('../src/View/blog/single_post.php');
+  require_once (__DIR__ .'/../src/View/blog/single_post.php');
 } else if (isset($_GET["tag"])) {
-  require_once ('../src/View/blog/tagged_posts.php');
+  require_once (__DIR__ .'/../src/View/blog/tagged_posts.php');
 } else {
-  require_once ('../src/View/blog/all_posts.php');
+  require_once (__DIR__ .'/../src/View/blog/all_posts.php');
 }
 
 
