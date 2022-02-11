ba97ba1e449e22c5ce41f9f1ff2dac307518b2d5
Tyler <44188028+twit96@users.noreply.github.com>
2022-02-11 14:01:53 -0600

    Update blog.php

--- src/Controller/blog.php
+++ src/Controller/blog.php
@@ -91,7 +91,6 @@ function getNumBlogPosts($db) {
 function configURL($db) {
   $ini = parse_ini_file(__DIR__ .'/../../config/config.ini.php', true)['blog_config'];
   $posts_per_page = $ini["posts_per_page"]
-  $ini = null; unset($ini);
 
   $total_pages = ceil(
     getNumBlogPosts($db) / $posts_per_page
