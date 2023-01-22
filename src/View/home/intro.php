53fe33467ae43746b4e8f3bb6228b551872abc38
Tyler <44188028+twit96@users.noreply.github.com>
2023-01-22 10:26:21 -0600

    Revert "Rewrite Homepage Intro Taglines"
    This reverts commit 964d7efba56389059ce0d8b6cd33d167889c2a7c.

--- src/View/home/intro.php
+++ src/View/home/intro.php
@@ -6,20 +6,12 @@
   <section id="intro-right">
     <h2>Well <i>hello</i> world,</h2>
     <p>
-      My name is Tyler. I am a full-stack web developer and...
+      My name is Tyler and I am a full-stack web developer. I specialize in...
     </p>
     <p class="taglines">
-      <span class="active tagline">Custom Website Connoisseur</span>
-      <span class="tagline">Website Redesign Wrangler</span>
-      <span class="tagline">Game Development Guru</span>
-      <span class="tagline">Frequent Fan of Food</span>
-      <span class="tagline">Dog <span style="text-decoration:underline;">and</span> Cat Lover &#128521;</span>
-      <span class="tagline">Zillennial &#9749;&#129331;&#128128;</span>
-      <span class="tagline">Are you still reading this?</span>
-      <span class="tagline">You should scroll down..</span>
-      <span class="tagline">Quick! Before it loops!</span>
-      <span class="tagline">NOOOOOOOOO!</span>
-      <span class="tagline">Well, this is awkward.</span>
+      <span class="active tagline">Custom Websites</span>
+      <span class="tagline">Website Redesigns</span>
+      <span class="tagline">Game Development</span>
     </p>
     <?php include(__DIR__ .'/../common/socials.html'); ?>
   </section>
